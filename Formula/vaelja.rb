ae2eb81e22380fc070e478660288a8b429787ad3
wadafacc <schlumpftimo14@gmail.com>
2026-04-16 15:06:59 +0200

    update sha256 in formula

--- Formula/vaelja.rb
+++ Formula/vaelja.rb
@@ -2,7 +2,7 @@ class Vaelja < Formula
   desc "Simple, PowerToys-inspired color picker for macOS"
   homepage "https://github.com/0xff4b/vaelja"
   url "https://github.com/0xff4b/vaelja/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "REPLACE_WITH_SHA256_AFTER_TAGGING"
+  sha256 "40121a3febcfe70e66560fdee997326ba62569d9be27c818fa0c81afecc1b168"
   license "MIT"
 
   depends_on macos: :ventura
