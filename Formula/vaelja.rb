06bf2d5e0c763a7ca0a55649ef69aa1168eaf3ce
wadafacc <schlumpftimo14@gmail.com>
2026-04-16 16:07:19 +0200

    bump sha

--- Formula/vaelja.rb
+++ Formula/vaelja.rb
@@ -2,7 +2,7 @@ class Vaelja < Formula
   desc "Simple, PowerToys-inspired color picker for macOS"
   homepage "https://github.com/0xff4b/homebrew-vaelja"
   url "https://github.com/0xff4b/homebrew-vaelja/archive/refs/tags/v1.0.4.tar.gz"
-  sha256 "db0756ffccc495cddf3ec75433a789739268054dbe374d90d7098d182a4ec7b1"
+  sha256 "08dd393c0c3de5aa03842a88520f063de4723991a4667aa98c672996e0c66b49"
   license "MIT"
 
   depends_on macos: :ventura
